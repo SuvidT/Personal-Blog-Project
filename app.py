1200d976ed04119d78be80363ebbe34705fadd91
Suvid Thungathurti <suvidthungathurti@Suvids-MacBook-Air.local>
2025-03-26 16:39:36 -0400

    possibly moments before success

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template
+import markdown
 from markupsafe import escape
 
 app = Flask(__name__)
 
-'''
-Random routes when we haven't decided anything
-'''
-@app.route("/user/<name>")
-def name(name):
-    return f"Hello, {escape(name)}"
+def render_md(filename):
+    with open(f"articles/{filename}", "r", encoding="utf-8") as f:
+        content = f.read()
+    return markdown.markdown(content)
 
-@app.route("/post/<int:id>")
-def post(id):
-    return f"Post: {escape(id)}"
+@app.route('/')
+def index():
+    return "<h1>This is the intro page</h1>"
 
-'''
-Important and main templates
-'''
 @app.route('/home')
 def home():
-    return "<h1>Personal Blod"
+    return "<h1>Personal Blog</h1>"
+
+@app.route('/article/<articleName>')
+def article(articleName):
+    # 1) check if file exists with OS
+    # 2) if not render a template
+    # 3) if yes then render it with the content template
+    # 4) make a content template in templates folder
+    pass
 
 ########## Execution of Websites ##########
 if __name__ == '__main__':
