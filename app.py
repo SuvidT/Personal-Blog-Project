7f4273966c9026c62591727d49b3a5cbc3da55dc
Suvid Thungathurti <suvidthungathurti@Suvids-MacBook-Air.local>
2025-03-26 23:36:08 -0400

    Final commit for the night before the push, I am satisfied with the articles page

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+########## File Reading Functions ##########
 def render_md(filename):
     with open(f"articles/{filename}.md", "r", encoding="utf-8") as f:
         content = f.read()
@@ -15,14 +16,22 @@ def render_json(filename):
     with open("articleDates.json", "r") as f:
         data = json.load(f)
     return filename, data[filename]
+############################################
 
+########## Routes and Pages ##########
+
+##### 404 page not found #####
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+##############################
 
+##### base route #####
 @app.route('/')
 def index():
     return "<h1>This is the intro page</h1>"
+######################
+
 
 @app.route('/home')
 def home():
@@ -30,17 +39,18 @@ def home():
 
 @app.route('/articles/<articleName>')
 def article(articleName):
-    # 1) check if file exists with OS
     file_path = f"articles/{escape(articleName)}.md"
+    
     if not exists(file_path):
-        # 2) if not render a template
         return render_template('404.html'), 404
 
-    # 3) if yes then render it with the content template
     content = render_md(articleName)
     title, date = render_json(articleName)
+
     return render_template("article.html", title=title, date=date, content=content)
+######################################
 
 ########## Execution of Websites ##########
 if __name__ == '__main__':
     app.run(debug=True)
+###########################################
